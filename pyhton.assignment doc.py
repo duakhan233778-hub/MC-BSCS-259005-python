cdbf59a783a7047e3ade482082c5d523da4c3b1e
duakhan233778-hub <duakhan233778@gmail.com>
2025-11-02 16:53:13 +0500

    python file
    work on VS CODE

---
+++ pyhton.assignment doc.py
@@ -0,0 +1,28 @@
+#many value to multi varaible
+x,y,z="apple","banana","mangoes"
+
+print(x)
+print(y)
+print(z)
+
+A singal varaible holding multiple value (composite data type):
+# Array (e.g., in Python)
+my_list = [1, 2, 3, 4]
+
+# Object/Structure (e.g., in Python)
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+person1 = Person("Alice", 30)
+
+# Unpacking a list
+city = ["multan", "lahore", "karachi"]
+x, y, z = city
+
+print(x)
+print(y)
+print(z)
+
+
